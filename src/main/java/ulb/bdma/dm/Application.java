7508ba6e07229f3870cdd564dfbb0af53ca83d01
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-11-10 11:04:06 +0100

    Initial Java and Gradle project structure

---
+++ src/main/java/ulb/bdma/dm/Application.java
@@ -0,0 +1,7 @@
+package ulb.bdma.dm;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/ulb/bdma/dm/ApplicationTest.java
@@ -0,0 +1,14 @@
+package ulb.bdma.dm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void main() {
+        Assertions.assertTrue(1 == 1);
+    }
+}
