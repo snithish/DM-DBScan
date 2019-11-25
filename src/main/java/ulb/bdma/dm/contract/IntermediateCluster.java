84b02de4f5f6b1c41f472254adbcae5f22a3b788
anantgupta_04 <anantgupta04@gmail.com>
2019-11-26 00:37:58 +0100

    Adding IntermediateCluster class file in CONTRACT

---
+++ src/main/java/ulb/bdma/dm/contract/IntermediateCluster.java
@@ -0,0 +1,9 @@
+package ulb.bdma.dm.contract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntermediateCluster {
+    List<IntermediateCluster> temp_cluster;
+    List<IntermediateCluster> unseen_points;
+}
